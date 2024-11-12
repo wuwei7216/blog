7d43eb10e45b4ece5a238c9c35e95bacefc4fc8d
wuwei <eric.w.wu@mail.foxconn.com>
2024-11-12 15:39:10 +0800

    docs: 添加live2d动画文档

--- src/.vitepress/catalog/Article.ts
+++ src/.vitepress/catalog/Article.ts
@@ -3,7 +3,8 @@ export const Article = [
     text: "实现一些小工具",
     collapsed: true,
     items: [
-      { text: "hitokoto 一言", link: "/docs/文章/tools/hitokoto" }
+      { text: "hitokoto 一言", link: "/docs/文章/tools/hitokoto" },
+      { text: "live2d", link: "/docs/文章/tools/live2d" },
     ],
   }
 ]
